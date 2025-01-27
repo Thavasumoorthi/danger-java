d5f96c883ca7c4be687d2b0e1480036c74a9aa6b
Harshini B <harshini.b@tringapps.com>
2025-01-27 15:26:34 +0530

    added some basic java

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,30 @@
 import {danger,fail} from 'danger'
 
+const  {execSync} =require('child_process')
+const { readFileSync } = require("fs");
 
 
-const modifiedFiles=danger.git.modified_files || danger.git.created_files
+
+let modifiedFiles=danger.git.modified_files || danger.git.created_files
+modifiedFiles=modifiedFiles.filter(file=>{
+    return file.endsWith(".java")
+})
+
+
+modifiedFiles.forEach((file=>{
+     const fileContent=readFileSync(file,'utf8');
+     console.log("+++File content is ",fileContent.split("\n"))
+     const modifiedfilecontent=fileContent.split("\n")
+     modifiedfilecontent.forEach((line=>{
+        if(line.includes("System.out.println"))
+        {
+            console.log("System keyword are present \t ",`file name is ${file}`,line)
+        }
+     }))
+
+}))
 
 
 console.log(modifiedFiles)
+
+
