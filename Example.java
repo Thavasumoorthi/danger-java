a4de008322d8b9148471e61001517c1b9fc7348d
Harshini B <harshini.b@tringapps.com>
2025-01-27 15:00:57 +0530

    added some file

--- Example.java
+++ Example.java
@@ -1,6 +1,13 @@
 public class Example{
+
+
+    public static void first()
+    {
+        System.out.println("first printoutstatement");
+    }
     public static void main(String[] args)
     {
         System.out.println("java is currently running");
+        first();
     }
 }
